81e5cbb6aa5f1066754e093bfb4baa5a825c73c0
alicsharp <alicsharp@hotmail.com>
2020-10-29 16:04:11 +0300

    site-packages added. main code changed.

--- examples/design.py
+++ examples/design.py
@@ -12,18 +12,18 @@ class DesignDLX(dlx.DLX):
 
         # Populate the columns variable.
         columns = list(pyncomb.ksubsetlex.all(v, t))
-        print(columns)
+        #print(columns)
         # Now create the rows, one for each k-set.
         rows = [[pyncomb.ksubsetlex.rank(v, T) for T in pyncomb.ksubsetlex.all(
             pyncomb.combfuncs.createLookup(S), t)] for S in pyncomb.ksubsetlex.all(v, k)]
-        print(rows)
-        print(len(rows))
+        #print(rows)
+        #print(len(rows))
         # Add a field to each column to indicate that it is primary.
         dlx.DLX.__init__(self, [(c, dlx.DLX.PRIMARY) for c in columns])
         self.rowsByLexOrder = self.appendRows(rows)
 
     def printSolution(self, solution):
-        return [list(set(reduce(lambda x,y:x+y, self.getRowList(i), []))) for i in solution]
+        return [list(set(reduce(lambda x, y: x + y, self.getRowList(i), []))) for i in solution]
 
 
 def solutions_list(design):
@@ -32,7 +32,7 @@ def solutions_list(design):
     new_list = []
     for d in design_list:
         new_list.append(design.printSolution(d))
-    print(f"---DESIGN ({v}, {k}, {1}) DONE ---".center(50,"-"))
+    print(f"---DESIGN ({v}, {k}, {1}) DONE ---".center(50, "-"))
     print("Number of designs found: %d" % len(design_list))
     r = (v - 1) / (k - 1)
     b = (v * r) / k
@@ -41,24 +41,37 @@ def solutions_list(design):
 
 
 design_731 = DesignDLX(2, 7, 3)
+design_931 = DesignDLX(2, 9, 3)
 
 sol_list_731 = solutions_list(design_731)
 
-perm = permutations(sol_list_731[0])
-
-# counting permunations and adding each permutation to a list. Permutations will return tuples @first.
-a = []
-count = 0
-for i in list(perm):
-    count += 1
-    a.append(i)
-
-# converting tuples to lists, because lists are easier to work with.
-perm_list = []
-for tuple in a:
-    temp_list = []
-    for item in tuple:
-        temp_list.append(item)
-    perm_list.append(temp_list)
+print(sol_list_731)
 
 
+def permuted_list(primitive_list):
+    perm = permutations(primitive_list)
+
+    # counting permunations and adding each permutation to a list. Permutations will return tuples @first.
+    a = []
+    count = 0
+    for i in list(perm):
+        count += 1
+        a.append(i)
+
+    # converting tuples to lists, because lists are easier to work with.
+    perm_list = []
+    for tuple in a:
+        temp_list = []
+        for item in tuple:
+            temp_list.append(item)
+        perm_list.append(temp_list)
+
+    return perm_list
+
+
+tempo_list = permuted_list(sol_list_731[0])
+
+print(tempo_list)
+print(len(tempo_list))
+
+# incidence structure, @sage ---> Automorphism Group
